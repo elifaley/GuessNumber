d96e043efe42e100068904e341a97a3fde73b0e0
elifaley <elifaley2@gmail.com>
2024-10-12 22:47:35 +0300

    AddGame

---
+++ game/game.cpp
@@ -0,0 +1,44 @@
+﻿// game.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+	int sayi = 0, girilenSayi = 0, denemeSayi = 0, denemeLimiti = 10;
+    int puan;
+
+	srand(time(NULL));
+	sayi = rand() % 100;
+
+    while (denemeSayi < denemeLimiti) {
+        cout << "Enter a number: ";
+        cin >> girilenSayi;
+
+        denemeSayi++;
+
+        if (sayi < girilenSayi) {
+            cout << "Enter a smaller number." << endl;
+        }
+        else if (sayi > girilenSayi) {
+            cout << "Enter a larger number." << endl;
+        }
+        else {
+            cout << sayi << "  is the number found in  " << denemeSayi << " attempts." << endl;
+            cout << "Congratulations!" << endl;
+            puan = (denemeLimiti - denemeSayi + 1) * 10;
+            cout << "Point: " << puan << endl;
+            break; 
+        }
+
+        if (denemeSayi == denemeLimiti) {
+            cout << "Unfortunately, you have run out of attempts. The correct number is: " << sayi << endl;
+        }
+    }
+
+    return 0;
+}
